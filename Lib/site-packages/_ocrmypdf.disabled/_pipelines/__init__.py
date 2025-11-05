52ec911e97c4c4eb481a74f53aadc9a497731c58
wodsyfounder-dotcom <wodsyfounder@gmail.com>
2025-11-05 16:37:48 -0700

    Stop tracking Lib site-packages

--- Lib/site-packages/_ocrmypdf.disabled/_pipelines/__init__.py
+++
@@ -1,5 +0,0 @@
-# SPDX-FileCopyrightText: 2023 James R. Barlow
-# SPDX-License-Identifier: MPL-2.0
-
-
-from __future__ import annotations
--- Lib/site-packages/_ocrmypdf.disabled/extra_plugins/__init__.py
+++
@@ -1,5 +0,0 @@
-# SPDX-FileCopyrightText: 2022 James R. Barlow
-#
-# SPDX-License-Identifier: MPL-2.0
-
-"""Extra plugins. These are not automatically inserted when ocrmypdf is run."""
